873ab64edf17920e88f8f0fc16e57798e646917a
Jay Varshney <jayvarshney30@gmail.com>
2026-07-14 19:19:31 +0530

    [1838] [Time Beats: 46.72%] [Memory Beats: 5.77%] - LeetPush

---
+++ 1838-Frequency-of-the-Most-Frequent-Element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int l = 0, r = 0, max = 0;
+        long sum = 0;
+
+        while(r < nums.length) {
+            sum += nums[r];
+
+            if(( (long) nums[r] * (r - l + 1) - sum) > k) {
+                sum -= nums[l];
+                l++;
+            }
+
+            max = Math.max(max, r - l + 1);
+            r++;
+        }
+        return max;
+    }
+}
