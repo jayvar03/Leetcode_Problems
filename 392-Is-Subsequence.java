32c614e65350165c09392ebc69682d6d3c1d4c18
Jay Varshney <jayvarshney30@gmail.com>
2026-07-16 17:14:49 +0530

    [392] [Time Beats: 95.68%] [Memory Beats: 25.64%] - LeetPush

---
+++ 392-Is-Subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        int m = s.length();
+        int n = t.length();
+
+        while(i < m && j < n) {
+            if(s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        if(i == m) return true;
+        else return false;
+    }
+}
