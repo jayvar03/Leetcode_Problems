8a265c084ea3e57f2697f0562f3a29ebc8f8d34b
Jay Varshney <jayvarshney30@gmail.com>
2026-07-16 18:26:43 +0530

    [2108] [Time Beats: 100.00%] [Memory Beats: 86.34%] - LeetPush

---
+++ 2108-Find-First-Palindromic-String-in-the-Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for(String word : words) {
+            if(isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+    private boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while(l <= r) {
+            if(s.charAt(l) != s.charAt(r)) {
+                return false;
+            } else {
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+}
