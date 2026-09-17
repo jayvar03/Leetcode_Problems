f8e77ab3b626f18624b5a7dd5f9893ee61bd4fc6
Jay Varshney <jayvarshney30@gmail.com>
2026-09-17 11:05:23 +0530

    [1456] [Time Beats: 33.04%] [Memory Beats: 65.60%] - LeetPush

--- 1456-Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.java
+++ 1456-Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.java
@@ -4,19 +4,15 @@ class Solution {
         int count = 0;
 
         for(int i = 0; i < k; i++) {
-            if(vowels.indexOf(s.charAt(i)) != -1) {
-                count++;
-            }
+            if(vowels.indexOf(s.charAt(i)) != -1) count++;
         }
-
+        
         int max = count;
 
         for(int i = k; i < s.length(); i++) {
             if(vowels.indexOf(s.charAt(i)) != -1) count++;
-
             if(vowels.indexOf(s.charAt(i - k)) != -1) count--;
-
-            max  = Math.max(count, max);
+            max = Math.max(count, max);
         }
         return max;
     }
