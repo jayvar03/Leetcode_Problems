a9a5375852c23cb3f58e0f3f372e974d1d1ad1f9
Jay Varshney <jayvarshney30@gmail.com>
2026-09-17 13:16:55 +0530

    [567] [Time Beats: 23.43%] [Memory Beats: 5.20%] - LeetPush

--- 567-Permutation-in-String.java
+++ 567-Permutation-in-String.java
@@ -1,29 +1,30 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
+        HashMap<Character, Integer> count1 = new HashMap<>();
+        HashMap<Character, Integer> count2 = new HashMap<>();
 
-        for(char c : s1.toCharArray()) {
-            freq1[c - 'a']++;
+        int k = s1.length();
+        if(k > s2.length()) return false;
+        
+        for(int i = 0; i < k; i++) {
+            count1.put(s1.charAt(i), count1.getOrDefault(s1.charAt(i), 0) + 1);
         }
 
-        int l = 0;
-
-        for(int r = 0; r < s2.length(); r++) {
-
-            freq2[s2.charAt(r) - 'a']++;
-
-            if(r - l + 1 > s1.length()) {
-                freq2[s2.charAt(l) - 'a']--;
-                l++;
-            }
-
-            if(r - l + 1 == s1.length()) {
-                if(Arrays.equals(freq1, freq2)) {
-                    return true;
-                }
-            }
+        for(int i = 0; i < k; i++) {
+            count2.put(s2.charAt(i), count2.getOrDefault(s2.charAt(i), 0) + 1);
         }
+
+        if(count1.size() == count2.size() && count1.equals(count2)) return true;
+
+        for(int i = k; i < s2.length(); i++) {
+            count2.put(s2.charAt(i - k), count2.get(s2.charAt(i - k)) - 1);
+            if(count2.get(s2.charAt(i - k)) == 0) count2.remove(s2.charAt(i - k));
+
+            count2.put(s2.charAt(i), count2.getOrDefault(s2.charAt(i), 0) + 1);
+
+            if(count1.size() == count2.size() && count1.equals(count2)) return true;
+        }
+
         return false;
     }
 }
